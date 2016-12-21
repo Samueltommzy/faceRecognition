088280f9328fef550c069c44c2d9fc6c5183d5e5
Ologunowa Samuel <samueloluwatommzy@gmail.com>
2016-12-21 19:25:34 +0100

    add files

---
+++ faceRec/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace faceRec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IFaceServiceClient faceServiceClient = new FaceServiceClient("bbc2011ffbfd450998d41215dbc102f7");
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+        private async Task<FaceRectangle[]> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream);
+                    var faceRects = faces.Select(face => face.FaceRectangle);
+                    return faceRects.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return new FaceRectangle[0];
+            }
+
+        }
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+
+            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
+            bool? result = openDlg.ShowDialog(this);
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto.Source = bitmapSource;
+
+        }
+
+       
+
+
+    
+    }
+}
+    
+
+       
